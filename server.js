68456ce1c38d81b259f392571048d351657f2f51
pro-masud <promasudbd@gmail.com>
2024-02-07 23:17:04 +0600

    server create a raw router in node js

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ const dotenv = require("dotenv").config();
 
 const PORT = process.env.PORT || 6061;
 http.createServer( (req, res) => {
+    if(req.url == "/" && req.method == "GET"){
+        res.writeHead('200', 'text/html');
+        res.write("<h1>Home</h1>")
+    }else if(req.url == "/about" && req.method == "GET"){
+        res.writeHead('200', 'text/html');
+        res.write("<h1>About</h1>");
+    }else if(req.url == "/blog" && req.method == "GET"){
+        res.writeHead('200', 'text/html');
+        res.write("<h1>Blog</h1>");
+    }else if(req.url == "/contact" && req.method == "GET"){
+        res.writeHead('200', 'text/html');
+        res.write("<h1>Contact Us</h1>");
+    }else{
+        res.writeHead('404', 'text/html');
+        res.write("<h1>404 Page</h1>");
+    }
     res.end();
 }).listen(PORT, () => {
     console.log(`Server Is Running Port Number ${PORT}`.bgGreen.black);
