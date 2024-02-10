7ebd7ca8bb7afb470d68e15d32725b5813eb4144
pro-masud <promasudbd@gmail.com>
2024-02-10 10:43:37 +0600

    express js default path module and express path module working successfully complete

--- server.js
+++ server.js
@@ -2,15 +2,32 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const path = require("path");
 
 const PORT = process.env.PORT || 6061;
 
 // express intilation
 const app = express();
 
-// file and folder public status 
-app.use(express.static('public'));
+// file and folder public status
+app.use(express.static("public"));
 
+// create express js file path
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/about.html"));
+});
+
+app.get("/shop", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/shop.html"));
+});
+
+app.get('/team', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/team-1.html'));
+})
 app.listen(PORT, () => {
-    console.log(`Server Is Running Port Number ${PORT}`.bgGreen.black);
+  console.log(`Server Is Running Port Number ${PORT}`.bgGreen.black);
 });
