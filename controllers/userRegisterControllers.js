22ba780fe628463689efbfeb9ffe82fbc73f4a2c
pro-masud <promasudbd@gmail.com>
2024-02-13 10:53:21 +0600

    sms send global vongage sms marketing agency

--- controllers/userRegisterControllers.js
+++ controllers/userRegisterControllers.js
@@ -1,8 +1,34 @@
 import axios from "axios";
 import nodemailler from "nodemailer";
+
+// register a vonage init here
+import { Vonage } from "@vonage/server-sdk";
+
+const vonage = new Vonage({
+  apiKey: "b02a3d1d",
+  apiSecret: "wgkZq63sii5JwQk2",
+});
+
+const from = "Vonage APIs";
+const to = "+8801797562295";
+const text = "A text message sent using the Vonage SMS API";
+
+async function sendSMS() {
+  await vonage.sms
+    .send({ to, from, text })
+    .then((resp) => {
+      console.log("Message sent successfully");
+      console.log(resp);
+    })
+    .catch((err) => {
+      console.log("There was an error sending the messages.");
+      console.error(err);
+    });
+}
+
 export const userRegister = async (req, res) => {
   // create a mailler transporter
-  const transport =  nodemailler.createTransport({
+  const transport = nodemailler.createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
     auth: {
@@ -11,16 +37,17 @@ export const userRegister = async (req, res) => {
     },
   });
 
-  // send mail transporter to gmail
- await transport.sendMail({
-    from: `LWMR Group ${process.env.MAIL_ADDRESS}`,
-    subject: "LWRM Students",
-    to: req.body.email,
-    text: `hello, I'm ${req.body.name} i'm a ${req.body.skills} Devs and i'm a ${req.body.age} Years Old`,
-  });
+    // send mail transporter to gmail
+   await transport.sendMail({
+      from: `LWMR Group ${process.env.MAIL_ADDRESS}`,
+      subject: "LWRM Students",
+      to: req.body.email,
+      text: `hello, I'm ${req.body.name} i'm a ${req.body.skills} Devs and i'm a ${req.body.age} Years Old`,
+    });
 
-  // create a axios package and send phone sms width in bulk sms bd 
-  await axios.get(`http://bulksmsbd.net/api/smsapi?api_key=cxVzYEy04o7WVqpJOf77&type=text&number= ${req.body.cell}&senderid=889612443880&message= HI ${req.body.name}, You are ${req.body.age} Years old and you are a ${req.body.skills}`);
+  //   // create a axios package and send phone sms width in bulk sms bd
+  //   await axios.get(`http://bulksmsbd.net/api/smsapi?api_key=cxVzYEy04o7WVqpJOf77&type=text&number= ${req.body.cell}&senderid=889612443880&message= HI ${req.body.name}, You are ${req.body.age} Years old and you are a ${req.body.skills}`);
 
+  sendSMS();
   res.status(200).json(console.log(req.body));
 };
